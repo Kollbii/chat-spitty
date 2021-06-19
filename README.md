6de842c8a638909740e0d19551d473f1c1dc0074
Kollbi <lkollbek@gmail.com>
2021-06-19 18:55:41 +0200

    Command
    $besos $hit

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Chat application or hidden game?
 
 # You gotta shove these seeds way up your butt Morty, waay up there. -Rick
 
+### Login
+Register - First login with unique username.
+![Login](assets/images/login.png)
+
+![Chat itself](assets/images/chat.png)
 
 # Not storing passwords! Only hashes!
+
 ### [...] I wonder what the secret key could look like...
--- post_commands.php
+++ post_commands.php
@@ -3,9 +3,18 @@ session_start();
 if(isset($_SESSION['name'])){
     $command = $_POST['command'];
     // TO DO SWITCH COMMAND MESSAGE OUTPUT
-    if ($command == '$hit'){
-        $textmsg = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> Uses ".stripslashes(htmlspecialchars($command))." command! <img src='https://media.giphy.com/media/HmgnQQjEMbMz0oLpqn/giphy.gif'><br></div>\n";
-        file_put_contents("log.html", $textmsg, FILE_APPEND | LOCK_EX);
+
+    switch($command){
+        case '$besos':
+            $textmsg = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> Uses ".stripslashes(htmlspecialchars($command))." command!</div>
+			<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> <img src='https://media.giphy.com/media/r4GoPRwcJU9Ak/giphy.gif'><br></div>\n";
+            file_put_contents("log.html", $textmsg, FILE_APPEND | LOCK_EX);
+            break;
+        case '$hit':
+            $textmsg = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> Uses ".stripslashes(htmlspecialchars($command))." command!</div>
+            		<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> <img src='https://media.giphy.com/media/HmgnQQjEMbMz0oLpqn/giphy.gif'><br></div>\n";
+	    file_put_contents("log.html", $textmsg, FILE_APPEND | LOCK_EX);
+            break;
     }
 }
 ?>
